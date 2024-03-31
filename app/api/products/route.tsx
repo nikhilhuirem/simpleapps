9763753ffb0e63643bfe307651d0b8fc8ccbf8ca
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-03-31 21:34:50 +0530

    chnages in api

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema  from "./schema";
 
 export function GET(request: NextRequest) {
     return NextResponse.json([
@@ -9,5 +10,8 @@ export function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
-    
+    const validation = schema.safeParse(body);
+    if(!validation.success)
+        return NextResponse.json({error: validation.error.errors}, { status: 400 });
+    return NextResponse.json({ id: 1, product: body.product, price: body.price}, { status : 201 })
 }
--- app/api/products/schema.ts
+++ app/api/products/schema.ts
@@ -1,9 +1,8 @@
 import { number, z } from 'zod';
 
 const schema = z.object({
-    id: z.number().positive(),
     name: z.string().min(3),
-    price: z.number().positive()
+    price: z.number().min(1).max(300)
 })
 
 export default schema;
